0713c52f664cc2a03a2f8e7a766d4480cec5234b
Nurgali <nrknrps@gmail.com>
2023-11-30 01:56:36 +0600

    final commit

--- script.py
+++ script.py
@@ -37,3 +37,61 @@ plt.ylabel('Number of People')
 plt.title('Number of People at Each Stress Level')
 
 plt.show()
+
+file_path = 'dataset.csv'
+df = pd.read_csv(file_path)
+
+average_sleep_duration_by_occupation = df.groupby('Occupation')['Sleep Duration'].mean().reset_index()
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Occupation', y='Sleep Duration', data=average_sleep_duration_by_occupation, palette='viridis')
+plt.xticks(rotation=45, ha='right')  
+plt.xlabel('Occupation')
+plt.ylabel('Average Sleep Duration')
+plt.title('Average Sleep Duration by Occupation')
+plt.show()
+
+
+average_stress_level_by_occupation = df.groupby('Occupation')['Stress Level'].mean().reset_index()
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Occupation', y='Stress Level', data=average_stress_level_by_occupation, palette='magma')
+plt.xticks(rotation=45, ha='right')  
+plt.xlabel('Occupation')
+plt.ylabel('Average Stress Level')
+plt.title('Average Stress Level by Occupation')
+plt.show()
+
+
+average_sleep_duration_by_age = df.groupby('Age')['Sleep Duration'].mean().reset_index()
+
+plt.figure(figsize=(12, 6))
+sns.barplot(x='Age', y='Sleep Duration', data=average_sleep_duration_by_age, palette='Blues')
+plt.xticks(rotation=45, ha='right')  
+plt.xlabel('Age')
+plt.ylabel('Average Sleep Duration')
+plt.title('Average Sleep Duration by Age')
+plt.show()
+
+
+average_quality_of_sleep_by_age = df.groupby('Age')['Quality of Sleep'].mean().reset_index()
+
+plt.figure(figsize=(12, 6))
+sns.barplot(x='Age', y='Quality of Sleep', data=average_quality_of_sleep_by_age, palette='Greens')
+plt.xticks(rotation=45, ha='right')
+plt.xlabel('Age')
+plt.ylabel('Average Quality of Sleep')
+plt.title('Average Quality of Sleep by Age')
+plt.show()
+
+bmi_category_by_occupation = df.groupby(['Occupation', 'BMI Category']).size().unstack(fill_value=0)
+bmi_category_by_occupation = bmi_category_by_occupation.div(bmi_category_by_occupation.sum(axis=1), axis=0)
+plt.figure(figsize=(12, 6))
+bmi_category_by_occupation.plot(kind='bar', stacked=True, colormap='viridis')
+plt.xticks(rotation=45, ha='right')
+plt.xlabel('Occupation')
+plt.ylabel('Proportion')
+plt.title('BMI Category Distribution by Occupation')
+plt.legend(title='BMI Category', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.show()
